ddebbcb0c98053d7dce20166cb3494252c5aceb0
LeanYinAi <yin_ai_lean@aiap.sg>
2025-01-07 21:46:06 +0800

    update init_script

--- aws_cloud/init_script.sh
+++ aws_cloud/init_script.sh
@@ -1,10 +1,9 @@
-# reference: https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-docker.html
 #!/bin/bash
-
 set -ex
 sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
+sudo chmod 666 /var/run/docker.sock
 sudo usermod -a -G docker ec2-user
 sudo yum install git -y
 sudo curl -L https://github.com/docker/compose/releases/download/v2.32.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
